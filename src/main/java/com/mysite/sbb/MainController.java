c94821bc43ac876828f413216a8b97d584d2c719
MADUPAL <kevin488@naver.com>
2025-11-19 15:32:06 +0900

    setting

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // 액션메서드를 만들기 위해 붙임
 public class MainController {
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/") //아래 메서드를 액션메서드로 만든다.
+    @ResponseBody    //아래 메서드의 리턴값을 브라우저로 보낸다.
     public String index() {
         System.out.println("index() 메서드 호출됨!");
         return "SBB";
